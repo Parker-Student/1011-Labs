ad885c96af46ae02c207eb84615c2f8353d864aa
Parker <parkerfoord@gmail.com>
2020-09-10 12:49:47 -0500

    added input - output

--- Labs1011/src/week01/MyFirstProgram.java
+++ Labs1011/src/week01/MyFirstProgram.java
@@ -1,8 +1,18 @@
 package week01;
 
+import java.util.Scanner;
+
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("Hello World");
+
+        Scanner keyboardInput = new Scanner(System.in);
+
+        System.out.println("Please input a number");
+
+        int number = keyboardInput.nextInt();
+        System.out.println("You entered " + number);
+
 
     }
 }
